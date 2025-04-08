613bc2a25b91068093cd22b7131b9b227278e88a
Codeer-man <mdrmoney34@gmail.com>
2025-04-08 22:06:21 +0545

    Managing frontend folder structure

---
+++ quiz/frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center w-screen">
+      <div>tesxt</div>
+    </div>
+  );
+}
